c81310e2d487389698d732b6846634bcb14402c4
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-01-19 20:23:08 -0300

    DTO de usuarios

--- src/main/java/com/springmongo/resources/UserResource.java
+++ src/main/java/com/springmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.springmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springmongo.domain.User;
+import com.springmongo.dtos.UserDTO;
 import com.springmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = service.findAll();
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(usersDTO);
 	}
 }
